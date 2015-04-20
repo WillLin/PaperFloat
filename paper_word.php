f66c6f82b0bdf02ba7eab1eaab961e4b914bb8e9
PavelEE <zaytsev.pavel.a@gmail.com>
2015-04-20 04:13:09 -0700

    paper word class

---
+++ paper_word.php
@@ -0,0 +1,70 @@
+<?php
+
+class Word
+{
+	public $word;
+	public $title;
+	public $authors;
+	public $conference;
+	public $link;
+
+	function __construct()
+	{
+		$this->authors = array();
+		
+	}
+
+	function setWord($word)
+	{
+		$this->word = $word;
+	}
+
+	function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	function setAuthor($author)
+	{
+		$this->authors[] = $author;
+	}
+
+	function copyAuthors($authors)
+	{
+		$this->authors = $authors;
+	}
+
+	function setConference($conference)
+	{
+		$this->conference = $conference;
+	}
+
+	function setLink($link)
+	{
+		$this->link = $link;
+	}
+
+
+	function getTitle(){
+		return $this->title;
+	}
+
+	function getAuthors(){
+		return $this->authors;
+	}
+	function getText(){
+		return $this->text;
+	}
+	function getLink(){
+		return $this->link;
+	}
+	function getConference(){
+		return $this->conference;
+	}
+
+	function getFrequency($word){
+		return substr_count($this->text, $word);
+	}
+}
+
+?>
